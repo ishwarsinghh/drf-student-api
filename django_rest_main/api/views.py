602d01eb1a62317c2eaf6148990b1dbecbe010dd
ishwarsinghh <ishwarsinghh26@gmail.com>
2026-05-25 14:45:09 +0530

    Refactor: Upgraded to Class-Based Generic Views

--- django_rest_main/api/views.py
+++ django_rest_main/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view 
 from rest_framework.views  import APIView
+from django.http import Http404
 # Create your views here.
 
 @api_view(['GET','POST'])
@@ -51,5 +52,35 @@ class Employees(APIView):
         employees=Employee.objects.all()
         serializer=EmployeeSerializer(employees,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def post(self,request):
+        serializer=EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND)
+    
+class EmployeeDetail(APIView):
+    def get_object(self,pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
         
-            
+    def get(self,request,pk):
+        emp=self.get_object(pk)
+        serializer=EmployeeSerializer(emp)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+        
+    def put(self,request,pk):
+        emp=self.get_object(pk)
+        serializer=EmployeeSerializer(emp,data=request.data)  
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        emp=self.get_object(pk)
+        emp.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
